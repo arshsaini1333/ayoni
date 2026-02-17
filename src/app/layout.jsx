6f811c53a054046fa69e633692a69cba8ca4b99f
Arshdeep Kaur <137913154+arshsaini1333@users.noreply.github.com>
2026-02-17 12:51:37 +0530

    Refactor Google Tag Manager integration in layout
    Updated Google Tag Manager implementation and removed Google Analytics script.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,33 +17,31 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={lato.className}>
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17857789550"
-          strategy="afterInteractive"
-        />
-        <Script id="google-ads-tag" strategy="afterInteractive">
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17857789550');
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W5GMMZ9F');
           `}
         </Script>
       </head>
-      <body className="antialiased">
-        <Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-G5SWEC7NT4"
-  strategy="afterInteractive"
-/>
 
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-G5SWEC7NT4');
-  `}
-</Script>
+      <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W5GMMZ9F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
 
         {children}
       </body>
