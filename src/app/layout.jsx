ea80e3402fa50463438445c3cebc01fd45de6621
arshsaini1333 <sainiarshdeep114@gmail.com>
2026-03-11 12:01:57 +0530

    new

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,44 +16,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={lato.className}>
-      <head>
-        {/* Google Tag Manager Script */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-W5GMMZ9F');
-          `}
-        </Script>
-        {/* Google Ads Global Site Tag */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17857789550"
-          strategy="afterInteractive"
-        />
-        <Script id="google-ads-gtag" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17857789550');
-          `}
-        </Script>
-      </head>
+      
+      {/* Google Tag Manager */}
+      <Script
+        src="https://www.googletagmanager.com/gtm.js?id=GTM-W5GMMZ9F"
+        strategy="afterInteractive"
+      />
+
+      {/* Google Ads */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17857789550"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-ads-gtag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17857789550');
+        `}
+      </Script>
 
       <body className="antialiased">
-        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-W5GMMZ9F"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+          />
         </noscript>
 
         {children}
